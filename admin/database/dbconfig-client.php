42a981e56e791fd6bd02748390e58bacf7bf12ea
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-24 05:07:31 -1200

    Update dbconfig-client.php

--- admin/database/dbconfig-client.php
+++ admin/database/dbconfig-client.php
@@ -1,3 +1,3 @@
 <?php  
-  $connection = mysqli_connect("localhost","root","","s6tm8jesl338iq5q");
+  $connection = mysqli_connect("nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","zpya0in3doc87txd","	w1h5pe97xwnoggrr","s6tm8jesl338iq5q");
 ?>
