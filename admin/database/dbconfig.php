3f658a4e31d27642e5bddf6c34910029cd454e6d
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-06-24 05:06:36 -1200

    Update dbconfig.php

--- admin/database/dbconfig.php
+++ admin/database/dbconfig.php
@@ -1,3 +1,3 @@
 <?php 
-  $connection = mysqli_connect("localhost","root","","codou_all_db");
+  $connection = mysqli_connect("nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","zpya0in3doc87txd","w1h5pe97xwnoggrr","s6tm8jesl338iq5q");
 ?>
